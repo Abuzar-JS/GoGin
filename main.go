c5f80ee65720fb411cf52ddd010cc8d1d552517f
Abuzar Jamshaid <54638297+Abuzar-JS@users.noreply.github.com>
2024-07-01 19:55:58 +0500

    Get Request

--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/PING", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/PING", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"Hello": "Abuzar from Lahore",
+			"Hello": "Abuzar G from Lahore",
 		})
 	})
-	r.Run(":8400")
+
+	router.GET("/abx/:id/:buyerID", func(c *gin.Context) {
+		id := c.Param("id")
+		fmt.Println(id)
+		buyerID := c.Param("buyerID")
+		c.JSON(http.StatusOK, gin.H{
+			"user_id": id,
+			"buyerID": buyerID,
+		})
+
+	})
+
+	router.POST("/test", func(c *gin.Context) {
+		// Email and Pass
+
+		type TestRequest struct {
+			Email string `json:"email" binding:"required"`
+			Pass  string `json:"pass"`
+		}
+		var req TestRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		// c.BindJSON(&req)
+		c.JSON(http.StatusOK, gin.H{
+			"email": req.Email,
+			"pass":  req.Pass,
+		})
+
+	})
+
+	router.Run(":8400")
 }
