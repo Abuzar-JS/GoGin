df22b8b06b4365c7101d3cbd52d34bd0f0c6447c
Abuzar Jamshaid <54638297+Abuzar-JS@users.noreply.github.com>
2024-07-12 21:29:55 +0500

    UpdateHashPass

--- internal/model/auth_model.go
+++ internal/model/auth_model.go
@@ -1,9 +1,9 @@
-package internal
+package model
 
 type UserAuth struct {
 	Id       int    `gorm:"primaryKey" json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:json:"email" validate:"required"`
+	Password string `json:"password" validate:"required,min=8,max=200"`
 }
 
 func (UserAuth) TableName() string {
--- services/auth_services.go
+++ services/auth_services.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"GinMod/internal/model"
 	internal "GinMod/internal/model"
+	"GinMod/utils"
 	"errors"
 
 	"gorm.io/gorm"
@@ -28,18 +30,22 @@ func (a *AuthService) Login(email *string, password *string) (*internal.UserAuth
 
 	var user internal.UserAuth
 
-	if err := a.db.Where("email=?", email).Where("password=?", password).Find(&user).Error; err != nil {
+	if err := a.db.Where("email=?", email).Find(&user).Error; err != nil {
 		return nil, err
 	}
 
+	if utils.CheckPasswordHash(*password, user.Password) == false {
+		return nil, errors.New("password not")
+	}
+
 	if user.Email == "" {
-		return nil, errors.New("No user found with this email.")
+		return nil, errors.New("no user found with this email")
 	}
 
 	return &user, nil
 }
 
-func (a *AuthService) Register(email *string, password *string) (*internal.UserAuth, error) {
+func (a *AuthService) Register(email *string, password *string) (*model.UserAuth, error) {
 	if email == nil {
 		return nil, errors.New("email can't be null")
 	}
@@ -48,10 +54,16 @@ func (a *AuthService) Register(email *string, password *string) (*internal.UserA
 		return nil, errors.New("password can't be empty")
 	}
 
-	var user internal.UserAuth
+	hashedPwd, err := utils.HashPassword(*password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user model.UserAuth
 
 	user.Email = *email
-	user.Password = *password
+	user.Password = hashedPwd
 
 	if err := a.db.Create(&user).Error; err != nil {
 		return nil, err
